6262d138a6f0d4dd42466f06c3b61e05bc23a71d
cbbdigital <cipribadic@gmail.com>
2026-03-29 13:06:10 +0300

    Increase maxWidth of layout container to 1200px

--- plugmarket 5/src/components/Layout.jsx
+++ plugmarket 5/src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ t, dark, setDark }) {
   return (
     <div style={{ background: t.bg, minHeight: "100vh", fontFamily: "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,sans-serif", color: t.tx }}>
       <Header t={t} />
-      <div style={{ maxWidth: 700, margin: "0 auto", padding: "66px 6% 80px" }}>
+      <div style={{ maxWidth: 1200, margin: "0 auto", padding: "66px 6% 80px" }}>
         <Outlet context={{ t, dark, setDark }} />
       </div>
       <BNav t={t} />
